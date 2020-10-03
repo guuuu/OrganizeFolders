0b59bb707e9552f22f5923fd5bef4fefd2f17853
Gustavo Nascimento <39128481+guuuu@users.noreply.github.com>
2020-10-03 02:45:07 +0100

    Add files via upload

--- uninstall pfo.bat
+++ uninstall pfo.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 SETLOCAL
-@RD /S /Q "%ProgramFiles%\PFO"
+@RD /S /Q "%ProgramFiles(x86)%\PFO"
 FOR /F "usebackq" %%f IN (`PowerShell -NoProfile -Command "Write-Host([Environment]::GetFolderPath('Desktop'))"`) DO (
   SET "DESKTOP_FOLDER=%%f"
   )
